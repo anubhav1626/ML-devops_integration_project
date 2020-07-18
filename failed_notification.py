36b5f1a3fc54ea35b27e1044aa824cdda67047c8
anubhav1626 <51701889+anubhav1626@users.noreply.github.com>
2020-07-18 15:59:32 +0530

    Update failed_notification.py

--- failed_notification.py
+++ failed_notification.py
@@ -1,26 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import smtplib 
 # creates SMTP session 
 s = smtplib.SMTP('smtp.gmail.com', 587) 
 # start TLS for security 
 s.starttls()   
 # Authentication 
-s.login("sender-mail", "password") 
+s.login("anubhavpahwa2608@gmail.com", "ayaehshcvikmnjgi") 
 # message to be sent 
 message = "Hey Developer, you need to check your code once. Might be this have some error. "
 # sending the mail 
-s.sendmail("sender-mail", "developer_mail", message) 
+s.sendmail("anubhavpahwa007@gmail.com", "anubhavpahwa2608@gmail.com", message) 
 # terminating the session 
 s.quit()
-
-
-# In[ ]:
-
-
-
-
